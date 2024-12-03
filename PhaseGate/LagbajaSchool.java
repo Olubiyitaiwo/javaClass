555db34b90c843cab7cd47f5970a00e3708a4d69
Olubiyitaiwo <olubiyipaul01@gmail.com>
2024-12-03 11:57:03 +0100

    second push

--- PhaseGate/LagbajaSchool.java
+++ PhaseGate/LagbajaSchool.java
@@ -6,7 +6,10 @@ public class LagbajaSchool{
 	public static void main(String[] args){
 	
 		studentDetails();
-		studentGrade();
+		studentGrade1();
+		studentGrade2();
+		studentGrade3();
+		studentGrade4();
 	
 
 	}
@@ -24,19 +27,71 @@ public class LagbajaSchool{
 		System.out.println("Saving>>>>>>>>>>>>>>>>>>>>>>");
 		System.out.println("Saved successfully");
 	}
-	public static void studentGrade(){
+	public static void studentGrade1(){
 		Scanner input = new Scanner(System.in);
 		
-		int[][] scores = new int[1][100];
-		System.out.print("Enter student: ");
+		int[][] scores = new int[1][3];
+		System.out.print("Enter students: ");
 		for(int counter = 0; counter < scores.length; counter++){
-			for(int index = 0; index < scores[counter].length; counter++){
+			for(int index = 0; index < scores[counter].length; index++){
 			System.out.println("Entering scores for student 1");
-			System.out.println("Enter scores for subject 1: ");
+			System.out.println("Enter scores for subjects : ");
 				scores[counter][index] = input.nextInt();
 		  }
 		}
-		System.out.println(Arrays.deepToString(scores));
+		System.out.println("Student 1: " + Arrays.deepToString(scores));
+		System.out.println("Saving>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println("Saved successfully");
 	}
 
+	public static void studentGrade2(){
+		Scanner input = new Scanner(System.in);
+		
+		int[][] scores = new int[1][3];
+		System.out.print("Enter students: ");
+		for(int counter = 0; counter < scores.length; counter++){
+			for(int index = 0; index < scores[counter].length; index++){
+			System.out.println("Entering scores for student 2");
+			System.out.println("Enter scores for subjects: ");
+				scores[counter][index] = input.nextInt();
+		  }
+		}
+		System.out.println("Student 2: " + Arrays.deepToString(scores));
+		System.out.println("Saving>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println("Saved successfully");
+	}
+
+		public static void studentGrade3(){
+		Scanner input = new Scanner(System.in);
+		
+		int[][] scores = new int[1][3];
+		System.out.print("Enter student: ");
+		for(int counter = 0; counter < scores.length; counter++){
+			for(int index = 0; index < scores[counter].length; index++){
+			System.out.println("Entering scores for student 3");
+			System.out.println("Enter scores for subject : ");
+				scores[counter][index] = input.nextInt();
+		  }
+		}
+		System.out.println("Student 3:" + Arrays.deepToString(scores));
+		System.out.println("Saving>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println("Saved successfully");
+	}
+	
+	public static void studentGrade4(){
+		Scanner input = new Scanner(System.in);
+		
+		int[][] scores = new int[1][3];
+		System.out.print("Enter student: ");
+		for(int counter = 0; counter < scores.length; counter++){
+			for(int index = 0; index < scores[counter].length; index++){
+			System.out.println("Entering scores for student 4");
+			System.out.println("Enter scores for subject : ");
+				scores[counter][index] = input.nextInt();
+		  }
+		}
+		System.out.println("Student 4:" + Arrays.deepToString(scores));
+		System.out.println("Saving>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println("Saved successfully");
+	}
 }
